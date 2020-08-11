6c91e2f8658171fb2f448f9a58f3d048441b6e0c
mbantra <36677005+mbantra@users.noreply.github.com>
2020-08-10 21:04:26 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const EventEmitter = require('events');
+
+class Server extends EventEmitter {
+    constructor(client) {
+        super();
+        client.on('command', (command) => {
+            console.log(`Command: ${command}`);
+            //commands to be implemented: add, list, delete
+            /*
+                Add command adds a task into the task array
+                List command returns the tasks in the array
+                Delete command removes the tasks from the array.
+            */ 
+        });
+    }
+}
+
+module.exports = (client) => new Server(client);
