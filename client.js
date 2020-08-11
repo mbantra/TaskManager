3d1ce76997438b3b7ffcd767e6e47edb35aaf2fd
mbantra <36677005+mbantra@users.noreply.github.com>
2020-08-10 21:04:57 -0400

    Add files via upload

---
+++ client.js
@@ -0,0 +1,14 @@
+const EventEmitter = require('events');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const client = new EventEmitter();
+const server = require('./server')(client);
+
+rl.on('line', (input) => {
+    client.emit('command', input);
+});
